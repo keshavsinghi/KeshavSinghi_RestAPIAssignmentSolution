6dd7af8d410cebcd7afd5b135ffd1fb36c2923a0
keshavsinghi <keshavsinghi.ks@gmail.com>
2022-04-19 23:55:13 +0530

    first commit

---
+++ src/main/resources/my.sql
@@ -0,0 +1,3 @@
+ insert into roles values('1','ADMIN');
+ INSERT INTO USERS VALUES('1','$2a$12$eIOaFbWC6q6iO3kPumlPxu0GQcffuW1nMhUmteh.NjqHWzliIfmiK','user1');
+ insert into users_roles values(1,1);
